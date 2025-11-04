987dadec76b2da4ca2cff1d4169a43a1786fe864
anderudp <pappandrasdavid@gmail.com>
2025-11-04 11:52:29 +0000

    ros distro fix

--- .vscode/source_ros2.sh
+++ .vscode/source_ros2.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-if [ -f /opt/ros/humble/setup.bash ]; then
-    source /opt/ros/humble/setup.bash
+if [ -f /opt/ros/jazzy/setup.bash ]; then
+    source /opt/ros/jazzy/setup.bash
 fi
 
 if [ -f "${ROS_WORKSPACE}/install/setup.bash" ]; then
