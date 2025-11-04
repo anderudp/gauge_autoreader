9f60d6f868bb5fd281ee9498926f734ca492bc7a
anderudp <pappandrasdavid@gmail.com>
2025-11-04 11:58:41 +0000

    aaaaaaaaa

--- .vscode/source_ros2.sh
+++ .vscode/source_ros2.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
-if [ -f /opt/ros/jazzy/setup.bash ]; then
-    source /opt/ros/jazzy/setup.bash
+if [ -f ~/.bashrc ]; then
+  source ~/.bashrc
 fi
 
-if [ -f "${ROS_WORKSPACE}/install/setup.bash" ]; then
-    source "${ROS_WORKSPACE}/install/setup.bash"
+if [ -f /opt/ros/jazzy/setup.bash ]; then
+  source /opt/ros/jazzy/setup.bash
 fi
+
+if [ -f "${PWD}/install/setup.bash" ]; then
+  source "${PWD}/install/setup.bash"
+fi
+
+echo "[ROS2] Environment sourced from: ${PWD}"
