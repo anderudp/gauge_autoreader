efadb13d0b195a85cfc2280649e9d93f1dd0538b
anderudp <pappandrasdavid@gmail.com>
2025-11-07 10:47:56 +0000

    calibrator work

--- src/gauge_control/setup.py
+++ src/gauge_control/setup.py
@@ -11,10 +11,10 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=['setuptools', 'pynput'],
     zip_safe=True,
     maintainer='root',
-    maintainer_email='pappandrasdavid@gmail.com',
+    maintainer_email='todo@todo.todo',
     description='TODO: Package description',
     license='MIT',
     extras_require={
@@ -24,7 +24,8 @@ setup(
     },
     entry_points={
         'console_scripts': [
-            'calibrate = gauge_control.calibrate:main'
+            'calibrator_node = gauge_control.calibrator_node:main',
+            'read_write_node = gauge_control.read_write_node:main'
         ],
     },
 )
