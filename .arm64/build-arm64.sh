7fc5bdd8916ab30e35c43f1d01967adca4839df5
anderudp <pappandrasdavid@gmail.com>
2026-03-20 06:41:07 +0000

    niced up arm build script a bit

--- .arm64/build-arm64.sh
+++ .arm64/build-arm64.sh
@@ -3,7 +3,8 @@ set -e
 cd /root/ws
 mkdir -p build-arm64
 
-docker run --rm --privileged tonistiigi/binfmt --install all
+docker run --rm --privileged tonistiigi/binfmt --install all  # registers emulators for buildx
+
 docker buildx build \
     --platform linux/arm64 \
     -t gar_rpi_gauge_builder:arm64 \
@@ -11,4 +12,5 @@ docker buildx build \
     --output type=local,dest=build-arm64 \
     --load \
     .
+
 mv build-arm64/install.tar.gz build-arm64/install_$(date +%Y%m%d_%H%M%S).tar.gz
